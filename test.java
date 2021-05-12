df5e7e63d456dbd0e98fc3f24a02ee1c7551304a
Aadish Jain <aadishjain1946@gmail.com>
2021-05-12 21:18:26 +0530

    today's code

--- test.java
+++ test.java
@@ -2,19 +2,51 @@ import java.util.*;
 
 public class test {
 
+  //product class to store product data
+  public static class product {
+
+    public String productName;
+    public int price;
+
+    //constructor to initialize product object
+    product(String productName, int price) {
+      this.productName = productName;
+      this.price = price;
+    }
+
+    //function to return name of product
+    String getProductName() {
+      return this.productName;
+    }
+
+    //function to return price of product after calculating total price
+    double getProductPrice() {
+      double ans = this.price;
+      if (ans > 10000) {
+        ans -= ans * 0.05;
+      }
+      double vat = ans * 0.14;
+      return ans + vat;
+    }
+  }
+
   public static void main(String args[]) {
-    //initializing variables
-    String title = "ABCD", last_name = "Roosevelt", first_name =
-      "Franklin", address = "xyz 5th St def ghi";
+    Scanner scnObj = new Scanner(System.in);
+    //prompt user to enter product name and product price
+    System.out.println("INPUT");
+    System.out.print("Enter Product Name: ");
+    String name = scnObj.nextLine();
+    System.out.print("Enter Product Price: ");
+    int price = scnObj.nextInt();
 
-    int student_number = 1234556789, age = 27;
-
-    //printing all the values of above variables
-    System.out.println("Title: " + title);
-    System.out.println("First Name: " + first_name);
-    System.out.println("Last Name: " + last_name);
-    System.out.println("Address: " + address);
-    System.out.println("Age: " + age);
-    System.out.println("Student Number: " + student_number);
+    //creating new product object
+    product prd1 = new product(name, price);
+    
+    //printing output to screen
+    System.out.println("\nOUTPUT");
+    System.out.println("Product Name: " + prd1.getProductName());
+    System.out.println(
+      "Product Price including VAT: " + prd1.getProductPrice()
+    );
   }
 }
