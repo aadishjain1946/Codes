c52ed65b971220dd58fbda6159156de0fd4f9afc
Aadish Jain <aadishjain1946@gmail.com>
2021-05-13 23:10:20 +0530

    c++ changes

--- test.cpp
+++ test.cpp
@@ -1,23 +1,44 @@
 #include <iostream>
-#include <fstream>
 using namespace std;
+int n = 5, m = 5;
+int visited[5][5];
+int x_corr[] = {1, -1, 0, 0};
+int y_corr[] = {0, 0, 1, -1};
+//function to validate coordinates
+bool checkBoundary(int i, int j)
+{
+    return (i >= 0 && i < n && j >= 0 && j < m);
+}
+//recursive funtion return total number of ways in which mouse can reach cheese.
+int returnNumberPaths(int i, int j)
+{
+    //if mouse reach at cheese we return 0
+    if (i == n - 1 && j == m - 1)
+    {
+        return 0;
+    }
+    int ans = 0;
+    visited[i][j] = 1;
+    //making recursive calls on all possible paths
+    for (int k = 0; k < 4; k++)
+    {
+        int newi = i + x_corr[k];
+        int newj = j + y_corr[k];
+        if (checkBoundary(newi, newj) && !visited[newi][newj])
+        {
+            ans += returnNumberPaths(newi, newj);
+        }
+    }
+    return ans + 1;
+}
 int main(int argc, char *argv[])
 {
-    for (int i = 1; i < argc; i++)
+    for (int i = 0; i < n; i++)
     {
-        string data;
-        //creating file object
-        ifstream fileObject;
-        //opening the file, name given as command line argument
-        fileObject.open(argv[i]);
-        cout << "\n"
-             << i << ") Printing data of file " << argv[i] << '\n';
-        //reading file data
-        while (fileObject && !fileObject.eof())
+        for (int j = 0; j < m; j++)
         {
-            getline(fileObject, data);
-            cout << data << endl;
+            visited[i][j] = 0;
         }
-        fileObject.close();
     }
+    cout << "NUmber of ways to reach cheese: " << returnNumberPaths(3, 3) << '\n';
 }
