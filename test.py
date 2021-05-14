a40e14b30dcd4c9dea595fbfeee180941f485860
Aadish Jain <aadishjain1946@gmail.com>
2021-05-14 21:11:24 +0530

    python changes

--- test.py
+++ test.py
@@ -1,8 +1,30 @@
-a = 10
-print("a:", type(a))
+# Taking input from user
+print("INPUT:")
+N, updt = (int(i) for i in input().split(" "))
+updateInp = [[int(i) for i in input().split(" ")] for k in range(updt)]
+# initializing s0
+s0 = ""
+for i in range(N):
+    s0 += "0"
+# outputString to store the lexicographically largest string
+outputString = ""
+# generating string for all given ranges
+for i in range(updt):
+    for j in range(updateInp[i][0], updateInp[i][1]+1):
+        if s0[j-1] == "1":
+            if j == len(s0):
+                s0 = s0[:j-1] + "0"
+                continue
+            s0 = s0[:j-1] + "0" + s0[j:]
+        else:
+            if j == len(s0):
+                s0 = s0[:j-1] + "1"
+                continue
+            s0 = s0[:j-1] + "1" + s0[j:]
 
-b = 10.5
-print("b:", type(b))
-
-c = "abcd"
-print("c:", type(c))
+    # updating outputString
+    if s0 > outputString:
+        outputString = s0
+# printing output
+print("OUTPUT:")
+print(outputString)
