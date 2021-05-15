210149691475d55b6b3437949825e966363ba30d
Aadish Jain <aadishjain1946@gmail.com>
2021-05-15 20:22:01 +0530

    c++ changes

--- test.cpp
+++ test.cpp
@@ -1,44 +1,63 @@
 #include <iostream>
+#include <string>
 using namespace std;
-int n = 5, m = 5;
-int visited[5][5];
-int x_corr[] = {1, -1, 0, 0};
-int y_corr[] = {0, 0, 1, -1};
-//function to validate coordinates
-bool checkBoundary(int i, int j)
+const int WIDTH = 5;
+const int HEIGHT = 5;
+int x_cor[] = {0, 0, 1, -1};
+int y_cor[] = {1, -1, 0, 0};
+bool checkCoordinates(int x, int y)
 {
-    return (i >= 0 && i < n && j >= 0 && j < m);
+    return (x >= 0 && x < HEIGHT && y >= 0 && y < WIDTH);
 }
-//recursive funtion return total number of ways in which mouse can reach cheese.
-int returnNumberPaths(int i, int j)
+void check(int x, int y, int dest_x, int dest_y,
+           int (&maze)[HEIGHT][WIDTH], std::string &path, int (&visited)[HEIGHT][WIDTH])
 {
-    //if mouse reach at cheese we return 0
-    if (i == n - 1 && j == m - 1)
+    //   if (x < 0 || y < 0 || x >= WIDTH|| y >= HEIGHT || !maze[y][x]) {
+    //     return;
+    //   }
+    if (x == dest_x && y == dest_y)
     {
-        return 0;
+        std::cout << path;
+        std::cout << x << ","<<y << '\n';
+        return;
     }
-    int ans = 0;
-    visited[i][j] = 1;
-    //making recursive calls on all possible paths
+    visited[x][y] = 1;
+    int len = path.size();
+    path += (char)('0' + x);
+    path += ',';
+    path += (char)('0' + y);
+    path += " > ";
     for (int k = 0; k < 4; k++)
     {
-        int newi = i + x_corr[k];
-        int newj = j + y_corr[k];
-        if (checkBoundary(newi, newj) && !visited[newi][newj])
+        int newi = x + x_cor[k];
+        int newj = y + y_cor[k];
+        if (checkCoordinates(newi, newj) && maze[newi][newj] == 1 && visited[newi][newj] == 0)
         {
-            ans += returnNumberPaths(newi, newj);
+            check(newi, newj, dest_x, dest_y, maze, path, visited);
+            visited[newi][newj] = 0;
         }
     }
-    return ans + 1;
+
+    path.resize(len);
 }
-int main(int argc, char *argv[])
+
+int main()
 {
-    for (int i = 0; i < n; i++)
+    int maze[HEIGHT][WIDTH] = {
+        {1, 1, 1, 1, 1},
+        {1, 1, 1, 0, 1},
+        {0, 1, 1, 0, 1},
+        {0, 1, 0, 0, 1},
+        {0, 1, 1, 1, 1}};
+    int visited[HEIGHT][WIDTH];
+    for (int i = 0; i < HEIGHT; i++)
     {
-        for (int j = 0; j < m; j++)
+        for (int j = 0; j < WIDTH; j++)
         {
             visited[i][j] = 0;
         }
     }
-    cout << "NUmber of ways to reach cheese: " << returnNumberPaths(3, 3) << '\n';
+    std::string path;
+    check(0, 0, 4, 4, maze, path, visited);
+    return 0;
 }
