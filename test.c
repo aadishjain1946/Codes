a86ee68bb39c6f2d5beda280f67477d7a49162fd
Aadish Jain <aadishjain1946@gmail.com>
2021-06-15 22:19:09 +0530

    more changes

--- test.c
+++ test.c
@@ -1,89 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-//function to check if given row indexes are valid or not
-int check(int n, int m, int i, int j)
-{
-    return i >= 0 && j >= 0 && i < n && j < m;
-}
 int main()
 {
-    FILE *filePointer;
-    char c;
-    //creating file object
-    filePointer = fopen("data.txt", "r");
-    if (filePointer == NULL)
+    //declaring 2d array
+    double data[50][5];
+    int n = 0;
+    double inp;
+    //for loop to rwad and store element in 2d array
+    for (int i = 0; i < 50; i++)
     {
-        printf("Cannot open file \n");
-        exit(0);
-    }
-    char sudo[100];
-    int k = 0, l = 0, m = 0;
-    double dataArray[7][4];
-    //reading data file from file object
-    c = fgetc(filePointer);
-    while (c != EOF)
-    {
-        //storing data into 2d array
-        if (c == ' ' || c == '\n')
+        //prompt user after every 5 iterations
+        if (i % 5 == 0)
         {
-            sudo[k] = '\0';
-            double temp = atof(&sudo[0]);
-            dataArray[m][l % 4] = temp;
-            l++;
-            if (l % 4 == 0 && l != 0)
-                m++;
-            k = 0;
+            //ask user if s/he wants to continue or not
+            if (i != 0)
+            {
+                n++;
+                int check;
+                printf("If you want to stop please enter 0, to continue enter 1: ");
+                scanf("%d", &check);
+                if (check == 0)
+                {
+                    break;
+                }
+            }
+            printf("Enter 5 items to store: ");
         }
-        sudo[k] = c;
-        k++;
-        c = fgetc(filePointer);
+        scanf("%lf", &inp);
+        data[n][i % 5] = inp;
     }
-    fclose(filePointer);
-
-    sudo[k] = '\0';
-    double temp = atof(&sudo[0]);
-    dataArray[m][l % 4] = temp;
-    //printing data in 2d array
-    printf("The content of the array is:\n");
-    for (int i = 0; i < 7; i++)
+    //printing the final array
+    printf("Entered items are: \n");
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < 4; j++)
+        for (int j = 0; j < 5; j++)
         {
-            printf("%f ", dataArray[i][j]);
-        }
-        printf("\n");
-    }
-    //reading row indexes from user
-    int index1, index2;
-    printf("Enter two row indexes to swap: ");
-    scanf("%d", &index1);
-    scanf("%d", &index2);
-    while (check(7, 7, index1, index2) == 0)
-    {
-        printf("Enter two row indexes to swap: ");
-        scanf("%d", &index1);
-        scanf("%d", &index2);
-    }
-    //swaping two rows using temporary array
-    double tempArr[4];
-    for (int i = 0; i < 4; i++)
-    {
-        tempArr[i] = dataArray[index1][i];
-    }
-    for (int i = 0; i < 4; i++)
-    {
-        dataArray[index1][i] = dataArray[index2][i];
-    }
-    for (int i = 0; i < 4; i++)
-    {
-        dataArray[index2][i] = tempArr[i];
-    }
-    printf("The content of the array AFTER the swap operations:\n");
-    for (int i = 0; i < 7; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            printf("%f ", dataArray[i][j]);
+            printf("%lf ", data[i][j]);
         }
         printf("\n");
     }
