08b79066f8999b68340cdfbd495155d9d038302f
Aadish Jain <aadishjain1946@gmail.com>
2021-06-20 11:44:39 +0530

    changes

--- test.c
+++ test.c
@@ -1,88 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-//student structure to store student information
-struct StudInfo
+int main(void)
 {
-    long stNr;
-    char Name[10];
-    int grade;
-};
-//function to list student data
-void list(struct StudInfo student)
-{
-    printf("1. Student Number: %d\n", student.stNr);
-    printf("2. Student Name: %s\n", student.Name);
-    printf("3. Student Grade: %d\n", student.grade);
-}
-//function to search for a particular student using binary search, given student number
-int binarySearch(long givenNr, struct StudInfo allStudent[], int n)
-{
-    int start = 0, end = n - 1, mid;
-    while (start <= end)
-    {
-        //calculating mid value
-        mid = (start + end) / 2;
-        //checking if student found at mid position or not
-        if (allStudent[mid].stNr == givenNr)
-        {
-            printf("Student Data Found: \n");
-            list(allStudent[mid]);
-            return 1;
-        }
-        //seraching on right half of the array
-        else if (allStudent[mid].stNr < givenNr)
-        {
-            start = mid + 1;
-        }
-        //seraching on left half of the array
-        else
-        {
-            end = mid - 1;
-        }
-    }
+
+    // Local Declarations
+    /**
+     * initially a int;
+     * Variable type should be before variable name
+     */
+    int a;
+    /**
+     * initially b :c :d char;
+     * Variable type should be before variable name
+     * and multiple variable should be saperated by commas(,)
+     */
+    char b, c, d;
+    /**
+     * initially d, e, f double float;
+     * Variable type should be before variable name
+     * and there can be single variable type for variables declared
+     */
+    double d, e, f;
+
+    // Statements
+
+    printf("The end of the program.");
+
     return 0;
-}
-//sorting the student array using bubble sort technique based on student name
-void sortBubble(struct StudInfo allStudent[], int n)
-{
-    struct StudInfo temp;
-    for (int j = 0; j < n - 1; j++)
-    {
-        for (int i = 0; i < n - j - 1; i++)
-        {
-            if (strcmp(allStudent[i].Name, allStudent[i + 1].Name) > 0)
-            {
-                temp = allStudent[i];
-                allStudent[i] = allStudent[i + 1];
-                allStudent[i + 1] = temp;
-            }
-        }
-    }
-}
-int main()
-{
-    //declaring student array with 5 students
-    struct StudInfo allStudent[5] = {{93790, "MRET", 77},
-                                     {963582, "ZEKI", 85},
-                                     {977434, "AHMED", 100},
-                                     {981229, "BAKI", 65},
-                                     {991280, "OSMAN", 88}};
-    int n = 5;
-    //taking input from user for student number to search
-    long givenNr;
-    scanf("%d", &givenNr);
-    int k = binarySearch(givenNr, allStudent, n);
-    if (k == 0)
-    {
-        printf("No Student found with student number %d\n", givenNr);
-    }
-    //sorting array
-    sortBubble(allStudent, n);
-    printf("\nStudent array after sorting it based on name\n");
-    for (int i = 0; i < n; i++)
-    {
-        printf("Student #%d: \n", i + 1);
-        list(allStudent[i]);
-    }
-}
+
+} // main
--- test.cpp
+++ test.cpp
@@ -1,148 +1,18 @@
 #include <iostream>
 using namespace std;
-//node class to store node data of BST
-class node
-{
-public:
-    int data;
-    node *left, *right;
-    node(int data)
-    {
-        this->data = data;
-        this->left = this->right = NULL;
-    }
-};
-//function to insert data into BST
-node *insert(node *head, int data)
-{
-    if (head == NULL)
-    {
-        node *newnode = new node(data);
-        return newnode;
-    }
-    if (head->data > data)
-    {
-        head->left = insert(head->left, data);
-    }
-    else
-    {
-        head->right = insert(head->right, data);
-    }
-    return head;
-}
-//funtion to print BST nodes in inorder
-void inorder(node *head)
-{
-    if (head == NULL)
-    {
-        return;
-    }
-    inorder(head->left);
-    cout << head->data << " ";
-    inorder(head->right);
-}
-//function to delete node form BST
-node *deleteBST(node *root, int data)
-{
-    if (root == NULL)
-    {
-        return NULL;
-    }
-    if (data > root->data)
-    {
-        root->right = deleteBST(root->right, data);
-        return root;
-    }
-    else if (data < root->data)
-    {
-        root->left = deleteBST(root->left, data);
-        return root;
-    }
-    else
-    {
-        if (root->left == NULL && root->right == NULL)
-        {
-            delete root;
-            return NULL;
-        }
-        else if (root->left == NULL)
-        {
-            node *tempNode = root->right;
-            root->right = NULL;
-            delete root;
-            return tempNode;
-        }
-        else if (root->right == NULL)
-        {
-            node *tempNode = root->left;
-            root->left = NULL;
-            delete root;
-            return tempNode;
-        }
-        else
-        {
-            node *tempNode = root->right;
-            while (tempNode->left != NULL)
-            {
-                tempNode = tempNode->left;
-            }
-            int minimumData = tempNode->data;
-            root->data = minimumData;
-            root->right = deleteBST(root->right, minimumData);
-            return root;
-        }
-    }
-}
 int main()
 {
-    //creating BST
-    node *n1 = new node(33);
-    node *n2 = new node(23);
-    node *n3 = new node(24);
-    node *n4 = new node(12);
-    node *n5 = new node(14);
-    node *n6 = new node(10);
-    node *n7 = new node(15);
-    node *n8 = new node(17);
-    node *n9 = new node(18);
-    node *n10 = new node(12);
-    node *n11 = new node(7);
-    node *n12 = new node(8);
-    node *n13 = new node(1);
-    node *n14 = new node(6);
-    node *n15 = new node(2);
-    n1->left = n2;
-    n2->left = n4;
-    n2->right = n3;
-    n4->left = n6;
-    n4->right = n5;
-    n6->left = n11;
-    n11->left = n13;
-    n11->right = n12;
-    n13->right = n14;
-    n14->left = n15;
-    n5->left = n10;
-    n5->right = n7;
-    n7->right = n8;
-    n8->right = n9;
-
-    //INSERTING 9, 16 inrot tree
-    cout << "Inserting 9 in BST\n";
-    n1 = insert(n1, 9);
-    cout << "Inserting 16 in BST\n";
-    n1 = insert(n1, 16);
-
-    cout << "\nPrinting Tree Data: ";
-    inorder(n1);
-
-    //Deleting 2,17,6 from BST
-    cout << "\n\nDeleting 2 from BST\n";
-    n1 = deleteBST(n1, 2);
-    cout << "Deleting 17 from BST\n";
-    n1 = deleteBST(n1, 17);
-    cout << "Deleting 6 from BST\n";
-    n1 = deleteBST(n1, 6);
-
-    cout << "\nPrinting Tree Data: ";
-    inorder(n1);
+    int denm = 4, numn = 3, n;
+    //prompt user to enter value of n
+    cout << "Enter value of n: ";
+    cin >> n;
+    double sum = 0;
+    //finding sum of series
+    for (int i = 0; i < n; i++)
+    {
+        sum += ((1.0) * numn / denm);
+        numn += 3;
+        denm += 4;
+    }
+    cout << "Sum of " << n << "th term is " << sum << '\n';
 }
