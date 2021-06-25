33ba665211547a2741e0582e2249967d6789cc11
Aadish Jain <aadishjain1946@gmail.com>
2021-06-25 15:25:49 +0530

    dfgdfgrtre

--- test.py
+++ test.py
@@ -1,36 +1,19 @@
-import random
-
-# function to return all the tempratures above freezing tempratures
-
-
-def getFreezingTemprature(tempratures):
-    return [i for i in tempratures if i > 32]
+def even(nu):
+    return nu % 2 == 0
 
 
-# creating an empty list
-tempratures = []
-# adding 24 random elements to the list
-for i in range(24):
-    tempratures.append(random.randint(10, 55))
+def adult(nu):
+    return nu >= 18
 
-tempratures = sorted(tempratures)
-# printing tempratures in ascending order
-print("Here are the tempratures, sorted asceding")
-for i in range(len(tempratures)):
-    print(tempratures[i], end=" ")
-# checking if temprature 32 is present in the list or not
-if 32 in tempratures:
-    print("\nThe first instance of 32 is at", tempratures.index(32))
-else:
-    tempratures.insert(4, 32)
-    print()
-# print first 6 elements in the list
-print("The first six elements are now", tempratures[:6])
-# calling function to get freezing tempratures
-freezingTemp = getFreezingTemprature(tempratures)
-freezingTemp = sorted(freezingTemp, reverse=True)
-# printing freezing tempratures in descending order
-print("The coldest temprature above freezing is", freezingTemp[-1])
-print("Here are the above freezing tempratures, sorted descending")
-for i in range(len(freezingTemp)):
-    print(freezingTemp[i], end=" ")
+
+jhon = 19
+terry = 18
+print(not (even(jhon) or adult(jhon)))
+print((not even(jhon)) and adult(jhon))
+print(even(jhon) and adult(jhon))
+print(not (even(jhon) and adult(jhon)))
+print((not even(jhon)) or (not adult(jhon)))
+print(even(terry) or adult(terry))
+print(even(terry) and adult(terry))
+print((not even(terry)) or adult(terry))
+print((not even(jhon)) or adult(jhon))
