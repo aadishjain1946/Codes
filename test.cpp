15b329ddf4085b49cc80fe6b5f1809eabc9293fb
Aadish Jain <aadishjain1946@gmail.com>
2021-07-03 08:24:02 +0530

    changes

--- test.cpp
+++ test.cpp
@@ -1,67 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-//time class to store time and do some operations
-class Time
-{
-    int hour, minute, second;
-
-public:
-    //default constructor to initialize object with value 0
-    Time()
-    {
-        this->hour = 0;
-        this->minute = 0;
-        this->second = 0;
-    }
-    //parametrized constructor to initialize object with givtn value
-    Time(int hour, int minute, int second)
-    {
-        this->hour = hour;
-        this->minute = minute;
-        this->second = second;
-    }
-    //function to set time
-    void setTime(int hour, int minute, int second)
-    {
-        this->hour = hour;
-        this->minute = minute;
-        this->second = second;
-    }
-    //function to print time in military format
-    void printMilitary()
-    {
-        cout << "Military Time: ";
-        cout << std::setw(2) << std::setfill('0') << this->hour << " : ";
-        cout << std::setw(2) << std::setfill('0') << this->minute << " : ";
-        cout << std::setw(2) << std::setfill('0') << this->second << '\n';
-    }
-    //function to print time in standard format
-    void printStandar()
-    {
-        int hours = this->hour;
-        string sudo = "AM";
-        if (hour > 12)
-        {
-            hour -= 12;
-            sudo = "PM";
-        }
-        cout << "Standard Time: ";
-        cout << std::setw(2) << std::setfill('0') << hour << " : ";
-        cout << std::setw(2) << std::setfill('0') << this->minute << " : ";
-        cout << std::setw(2) << std::setfill('0') << this->second << " " << sudo << '\n';
-    }
-};
 int main()
 {
-    //testing the time class
-    cout << "#T1\n";
-    Time T1(1, 5, 25);
-    T1.printMilitary();
-    T1.printStandar();
+    int n = 11;
+    int array[] = {698, 685, 692, 731, 728, 712, 738, 724, 689, 685, 682};
+    double mean = 0, median = 0;
+    map<int, int> freq;
+    // calculating mean of the given array
+    for (int i = 0; i < n; i++)
+    {
+        mean += array[i];
+        freq[array[i]] += 1;
+    }
+    mean /= n;
+    cout << "Mean: " << mean << "\n";
 
-    cout << "\n#T2\n";
-    Time T2(15, 25, 30);
-    T2.printMilitary();
-    T2.printStandar();
+    //calculating median of the given array
+    sort(array, array + n);
+    (n % 2 == 0) ? median = (array[(n / 2)] + array[(n / 2) + 1]) / 2 : median = array[(n / 2)];
+    cout << "Median: " << median << "\n";
+
+    //calculating mode of the gien array
+    int maxFreq = -1;
+    cout << "Mode: ";
+    for (auto i : freq)
+    {
+        maxFreq = max(maxFreq, i.second);
+    }
+    for (auto i : freq)
+    {
+        if (i.second == maxFreq)
+        {
+            cout << i.first << " ";
+        }
+    }
 }
 
--- test.py
+++ test.py
@@ -1,28 +1,11 @@
-def main():
-    name = "Abcd"
-    Id = 123
-    bookPrice = 10
-    # loop to perform operations 4 times
-    for i in range(4):
-        # prompt user to enter number of books and name
-        n = int(input("How many book you want to buy: "))
-        custName = input("Enter your name: ")
-        print("Customer Name:", custName)
-        totalCost = 0
-        # calculating book cost
-        if n > 100:
-            totalCost = n * bookPrice * 0.4
-            print("Output: Huge bulk")
-        elif n >= 50 and n <= 100:
-            totalCost = n * bookPrice * 0.2
-            print("Output: Medium bulk")
-            break
-        else:
-            totalCost = n * bookPrice * 0.05
-            print("Output: Small bulk")
+amount_to_change = int(input())
 
-        print("Total Cost:", totalCost)
+num_fives = amount_to_change // 5
 
+''' Your solution goes here '''
+# remaining bill should be 1 dollar bills
+num_ones = amount_to_change % 5
 
-if __name__ == "__main__":
-    main()
+print('Change for $', amount_to_change)
+print(num_fives, 'five dollar bill(s) and', num_ones, 'one dollar bill(s)')
+
