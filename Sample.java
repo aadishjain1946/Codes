35394495d1d49eb02db7c2116cda3cf49bf56df9
Aadish Jain <aadishjain1946@gmail.com>
2021-07-09 07:55:03 +0530

    changes

--- Sample.java
+++ Sample.java
@@ -1,38 +1,50 @@
 import java.util.*;
 
-public class Sample<T1, T2> {
+public class Sample {
 
+  //class to build max heap
   public static class HeamMax {
 
     private ArrayList<Integer> heap;
     private int size;
 
+    //constructor to initialize heap array with given size
     public HeamMax(int n) {
       this.size = n;
       this.heap = new ArrayList<Integer>(n);
     }
-    public HeamMax(){}
+
+    //default constructor to initialize object
+    public HeamMax() {
+      this.size = 0;
+      this.heap = new ArrayList<Integer>();
+    }
+
+    //fucntion to check if heap is empty or not
     public boolean isEmpty() {
       return heap.size() == 0;
     }
 
+    //function to display all heap element
     public void display() {
       System.out.print("Elements inside heap are: ");
       for (int i = 0; i < heap.size(); i++) {
-        System.out.print(heap[i] + " ");
+        System.out.print(heap.get(i) + " ");
       }
     }
 
+    //function to get max value from heap.
     public int getMax() {
       if (isEmpty()) {
-        return Integer.MIN_VALUE;
+        return -1;
       }
       return heap.get(0);
     }
 
+    //function to insert element in heap
     public void insert(int element) {
       if (heap.size() >= this.size) {
-        System.out.println("Cannot insert element in Heap.")
+        System.out.println("Cannot insert element in Heap.");
         return;
       }
       heap.add(element);
@@ -51,6 +63,7 @@ public class Sample<T1, T2> {
       }
     }
 
+    //function to remove top element from heap
     public int removeMax() {
       if (isEmpty()) {
         return Integer.MIN_VALUE;
@@ -66,10 +79,7 @@ public class Sample<T1, T2> {
         if (heap.get(leftCInd) > heap.get(maxind)) {
           maxind = leftCInd;
         }
-        if (
-          rightCInd < heap.size() &&
-          heap.get(rightCInd) > heap.get(maxind)
-        ) {
+        if (rightCInd < heap.size() && heap.get(rightCInd) > heap.get(maxind)) {
           maxind = rightCInd;
         }
         if (maxind == ind) {
@@ -85,12 +95,36 @@ public class Sample<T1, T2> {
       }
       return ans;
     }
-    public void build(int[] arr){
-      for(int i = 0;i<arr.length;i++){
+
+    //function to build heap using given array
+    public void build(int[] arr) {
+      for (int i = 0; i < arr.length; i++) {
         this.insert(arr[i]);
       }
     }
   }
 
-  public static void main(String[] args) {}
+  //function to return kth largest element in the matrix
+  public static int KthBiggest(int[][] matrix, int k) {
+    int n = matrix.length;
+    int m = matrix[0].length;
+    HeamMax h1 = new HeamMax(n * m);
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        h1.insert(matrix[i][j]);
+      }
+    }
+    for (int i = 0; i < k - 1; i++) {
+      h1.removeMax();
+    }
+    return h1.getMax();
+  }
+
+  public static void main(String[] args) {
+    int[][] matrix = { { 1, 5, 9 }, { 10, 11, 13 }, { 12, 13, 15 } };
+    int k = 2;
+    System.out.println(
+      "Kth Biggest element in the matrix is " + KthBiggest(matrix, k)
+    );
+  }
 }
