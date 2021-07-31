dcb801ef353bfc21a8248319541fad14eaff0ac4
aadishjain1946 <aadishjain1946@gmail.com>
2021-07-31 13:04:05 +0530

    changes

--- test.py
+++ test.py
@@ -1,11 +1,4 @@
-# prompt user to enter space saperated element of list
-arr = [int(i) for i in input("Input: ").split(" ")]
-# declaring result array to store odd element from given list
-result = []
-# using for loop to iterate over the input list
-# and storing odd element in the empty list declared above
-for i in range(len(arr)):
-    if arr[i] % 2 == 1:
-        result.append(arr[i])
-# printing output
-print("Output: ", result)
+count = 4
+while count < 12:
+    print("counting")
+    count = count + 2
