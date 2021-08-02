69dccbd1c3f94106768bb2c077e140687f87d431
aadishjain1946 <aadishjain1946@gmail.com>
2021-08-02 08:00:10 +0530

    changes

--- test.cpp
+++ test.cpp
@@ -1,51 +1,88 @@
+
 #include <iostream>
-#include <queue>
 using namespace std;
-int main()
+#define INFY 9999
+const int n = 5, s = 0, d = 3;
+#define NILL -1
+int getMin(int *arr, int *end)
 {
-    int arr[] = {60, 76, 45, 80, 62};
-    int n = sizeof(arr) / sizeof(int);
-    queue<int> srtQue, q1, q2;
-    // using 2 extra queue to sort the given list of department
-    for (int i = 0; i < n; i++)
-    {
-        if (srtQue.empty())
+    int min = INFY, min_index, v;
+    for (v = 0; v < n; v++)
+        if (!end[v] && arr[v] < min)
         {
-            srtQue.push(arr[i]);
+            min = arr[v];
+            min_index = v;
         }
+    return min == INFY ? INFY : min_index;
+}
+
+void PrintPath(int *parent, int _d)
+{
+    if (parent[_d] == NILL)
+    {
+        cout << _d;
+        return;
+    }
+    PrintPath(parent, parent[_d]);
+    cout << "->" << _d;
+}
+void Dijkstra(int Graph[n][n], int _n, int _s, int _d)
+{
+
+    int i, u, v, count;
+    int arr[n];
+    int end[n] = {0};
+    int pathlength[n] = {0};
+    int parent[n];
+
+    parent[_s] = NILL;
+
+    for (i = 0; i < n; i++)
+        arr[i] = INFY;
+
+    arr[_s] = 0;
+    for (count = 0; count < n - 1; count++)
+    {
+        u = getMin(arr, end);
+
+        if (u == INFY)
+            break;
         else
         {
-            //storing all element less than arr[i] in q1
-            while (srtQue.front() < arr[i])
+
+            end[u] = 1;
+            for (v = 0; v < n; v++)
             {
-                q1.push(srtQue.front());
-                srtQue.pop();
-            }
-            q1.push(arr[i]);
-            //storing all remaining element in q2
-            while (!srtQue.empty())
-            {
-                q2.push(srtQue.front());
-                srtQue.pop();
-            }
-            //putting back all element in srtQue queue
-            while (!q1.empty())
-            {
-                srtQue.push(q1.front());
-                q1.pop();
-            }
-            while (!q2.empty())
-            {
-                srtQue.push(q2.front());
-                q2.pop();
+                if (!end[v] && Graph[u][v] && arr[u] + Graph[u][v] < arr[v])
+                {
+                    parent[v] = u;
+                    pathlength[v] = pathlength[parent[v]] + 1;
+                    arr[v] = arr[u] + Graph[u][v];
+                }
+                else if (!end[v] && Graph[u][v] && arr[u] + Graph[u][v] == arr[v] && pathlength[u] + 1 < pathlength[v])
+                {
+                    parent[v] = u;
+                    pathlength[v] = pathlength[u] + 1;
+                }
             }
         }
     }
-    //printing output
-    cout << "Output: ";
-    while (!srtQue.empty())
-    {
-        cout << srtQue.front() << " ";
-        srtQue.pop();
-    }
+
+    if (arr[_d] != INFY)
+        PrintPath(parent, _d);
+    else
+        cout << "There is no path between vertex "
+             << _s << "to vertex " << _d;
+}
+
+int main()
+{
+
+    int Graph[n][n] = {{0, 1, INFY, INFY, 10},
+                       {1, 0, 4, INFY, INFY},
+                       {INFY, 4, 0, 7, INFY},
+                       {INFY, INFY, 7, 0, 2},
+                       {10, INFY, INFY, 2, 0}};
+    Dijkstra(Graph, n, s, d);
+    return 0;
 }
--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-count = 4
-while count < 12:
-    print("counting")
-    count = count + 2
+def paurSum(arr,n,target):
+    res = {}
+    for i in range(n):
+        if target - arr[i] in res:
+            return [res[target - arr[i]], i]
+        else:
+            res[arr[i]] = i
+
+arr = [int(i) for i in input("Enter Array Elements: ").split(" ")]
+target = int(input("Enter target: "))
+n = len(arr)
+print("Output: ",paurSum(arr,n,target))
+
