99ee2685ea307ce524b282971e47b11ba5688343
aadishjain1946 <aadishjain1946@gmail.com>
2021-08-18 08:24:45 +0530

    changes

--- test.cpp
+++ test.cpp
@@ -1,31 +1,95 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
-//function to check if the given num is a perfect square or not
-bool checkPerfctSqr(int num)
+//stack class to push and pop element in stack
+class stack
 {
-    int squr = sqrt(num);
-    return (squr * squr == num);
-}
-//function to check if the given number is fibonacci number or not
-bool checkFib(int num)
+    int top;
+    int *arr;
+    int size;
+
+public:
+    stack(int size)
+    {
+        top = -1;
+        arr = new int[size];
+        this->size = size;
+    }
+    void push(int ele)
+    {
+        if (size <= 0)
+        {
+            return;
+        }
+        top++;
+        arr[top] = ele;
+        size--;
+    }
+    int pop()
+    {
+        if (top == -1)
+        {
+            return -1;
+        }
+        int output = arr[top];
+        top--;
+        size++;
+        return output;
+    }
+};
+
+//queue class to push and pop element in queue
+class queue
 {
-    return checkPerfctSqr(5 * num * num + 4) || checkPerfctSqr(5 * num * num - 4);
-}
+    int front, rear;
+    int *arr;
+    int size;
+
+public:
+    queue(int size)
+    {
+        front = rear = 0;
+        arr = new int[size];
+        this->size = size;
+    }
+    void push(int ele)
+    {
+        if (size <= 0)
+        {
+            return;
+        }
+        arr[rear] = ele;
+        rear++;
+        size--;
+    }
+    int pop()
+    {
+        if (front == rear)
+        {
+            front = rear = 0;
+            return -1;
+        }
+        int output = arr[front];
+        front++;
+        size++;
+        return output;
+    }
+};
 int main()
 {
-    //prompt user to enter an integer
-    int num;
-    cout << "Enter a number: ";
-    cin >> num;
-    //prinit the result
-    if (checkFib(num))
-    {
-        cout << num << " is a Fibonacci Number \n";
-    }
-    else
-    {
-        cout << num << " is a not Fibonacci Number \n";
-    }
-    return 0;
+    stack s1(10);
+    cout << "Stack: \n";
+    s1.push(1);
+    s1.push(2);
+    s1.push(3);
+    cout << s1.pop() << "\n";
+    cout << s1.pop() << "\n";
+    cout << s1.pop() << "\n";
+    queue q1(10);
+    cout << "Queue: \n";
+    q1.push(1);
+    q1.push(2);
+    q1.push(3);
+    cout << q1.pop() << "\n";
+    cout << q1.pop() << "\n";
+    cout << q1.pop() << "\n";
 }
