fee2a0d9226e5166582792154e2fe1fb3d07766e
aadishjain1946 <aadishjain1946@gmail.com>
2021-08-20 18:31:31 +0530

    changes

--- test.java
+++ test.java
@@ -1,17 +1,40 @@
 import java.util.*;
 
 public class test {
-  public static class RetrieveRandomElmFromArrList{
-    public static Integer solution(ArrayList<Integer> arrList, int index){
-    // ↓↓↓↓ your code goes here ↓↓↓↓
-    int start = 0, end = arrList.size() - 1;
-    Random rand = new Random();
-    int ind = rand.nextInt(end-start) + start;
-    Integer output = new Integer(arrList.get(ind));
-    return 0;
+  //function to return sum of an array
+  static int getSum(int arr[],int n){
+      int sum = 0;
+      for(int j = 0;j<n;j++){
+        sum += arr[j];
+      }
+      return sum;
+  }
+  //function to return maximum sum after flipping necessary card
+  static int maxSum(int arr[],int i,int n,int flipCnt){
+    if(i>=n){
+      return getSum(arr,n);
     }
+    if(flipCnt > 0 && flipCnt < i){
+      return getSum(arr,n);
+    }
+
+    arr[i] *= -1;
+    int ans1 = maxSum(arr,i+1,n,flipCnt+1);
+    arr[i] *= -1;
+    int ans2 = maxSum(arr,i+1,n,flipCnt);
+    return Math.max(ans1,ans2);
   }
   public static void main(String[] args) {
-    
+    //prompt user to enter input
+    System.out.println("Input:");
+    Scanner scnObj = new Scanner(System.in);
+    int n = scnObj.nextInt();
+    int []arr = new int[n];
+    for(int i = 0;i<n;i++){
+      arr[i] = scnObj.nextInt();
+    }
+    //printing output
+    System.out.println("Output:");
+    System.out.println(maxSum(arr,0,n,0));
   }
 }
