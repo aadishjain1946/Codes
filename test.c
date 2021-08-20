abb0e9267a89ef4ce5fc12f08b86af9ed6eb8d68
aadishjain1946 <aadishjain1946@gmail.com>
2021-08-20 17:03:17 +0530

    changes

--- test.c
+++ test.c
@@ -1,23 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
-//recursive function to return number of odd element in an array
-int oddRec(int *arr, int n)
+typedef struct node
 {
-    if (n < 0)
+    int data;
+    node *next;
+} node;
+node *tail = NULL;
+node *insert(int data, node *head, node *tail)
+{
+    node *newNode = (node *)malloc(sizeof(node));
+    newNode->data = data;
+    newNode->next = NULL;
+    tail->next = newNode;
+    return newNode;
+}
+void print(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
     {
-        return 0;
+        printf("%d ", temp->data);
+        temp = temp->next;
     }
-    int ans = oddRec(arr, n - 1);
-    if (arr[n] % 2 != 0)
+}
+int removeRange(node **head, int min, int max)
+{
+    node *temp = (*head), *prev = NULL;
+    int cnt = 0;
+    while (temp != NULL)
     {
-        return ans + 1;
+        if (temp->data >= min && temp->data <= max)
+        {
+            if (prev == NULL)
+            {
+                node *sudo = (*head);
+                (*head) = (*head)->next;
+                temp = temp->next;
+                cnt++;
+                continue;
+            }
+            node *sudo = temp;
+            temp = temp->next;
+            prev->next = temp;
+            cnt++;
+            continue;
+        }
+        prev = temp;
+        temp = temp->next;
     }
-    return ans;
+    return cnt;
 }
 int main()
 {
-    int arr[] = {1, 2, 3, 4, 5};
-    int n = 5;
-    int ans = oddRec(arr, n - 1);
-    printf("Output: %d", ans);
+    node *head = (node *)malloc(sizeof(node));
+    head->data = 4;
+    head->next = NULL;
+    tail = head;
+    tail = insert(2, head, tail);
+    tail = insert(1, head, tail);
+    tail = insert(10, head, tail);
+    tail = insert(15, head, tail);
+    tail = insert(8, head, tail);
+    tail = insert(7, head, tail);
+    tail = insert(4, head, tail);
+    tail = insert(20, head, tail);
+    tail = insert(36, head, tail);
+    tail = insert(-3, head, tail);
+    tail = insert(40, head, tail);
+    tail = insert(5, head, tail);
+    print(head);
+    int noRem = removeRange(&head, 4, 20);
+    printf("\nOutput: %d\n", noRem);
+    print(head);
 }
