04bc52426e56633b2f3544e51fa9c97f54b51f31
aadishjain1946 <aadishjain1946@gmail.com>
2021-08-31 23:11:21 +0530

    changes

--- test.cpp
+++ test.cpp
@@ -1,18 +1,68 @@
 #include <iostream>
 using namespace std;
+class DoublyLinkedList
+{
+
+public:
+    int data;
+    DoublyLinkedList *next, *prev;
+    DoublyLinkedList(int data)
+    {
+        this->data = data;
+        this->next = this->prev = NULL;
+    }
+};
+void insert(DoublyLinkedList **head, DoublyLinkedList **tail, int data)
+{
+    // if ((*tail))
+    //     cout << (*tail)->data << '\n';
+    DoublyLinkedList *temp = new DoublyLinkedList(data);
+    if ((*head) == NULL && (*tail) == NULL)
+    {
+        (*head) = temp;
+        (*tail) = temp;
+    }
+    else
+    {
+        (*tail)->next = temp;
+        temp->prev = (*tail);
+        (*tail) = temp;
+    }
+}
+void print(DoublyLinkedList *head)
+{
+    DoublyLinkedList *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
 int main()
 {
-    string str;
-    cout << "Enter a string: ";
-    cin >> str;
-    char result = str[0];
-    for (int i = 1; i < str.length(); i++)
+    DoublyLinkedList *list1Head = NULL, *list1Tail = NULL, *list2Head = NULL, *list2Tail = NULL, *list3Head = NULL, *list3Tail = NULL;
+    insert(&list1Head, &list1Tail, 5);
+    insert(&list1Head, &list1Tail, 6);
+    insert(&list1Head, &list1Tail, 7);
+    insert(&list1Head, &list1Tail, 8);
+    insert(&list1Head, &list1Tail, 9);
+    insert(&list2Head, &list2Tail, 1);
+    insert(&list2Head, &list2Tail, 2);
+    insert(&list2Head, &list2Tail, 3);
+    insert(&list2Head, &list2Tail, 4);
+    insert(&list2Head, &list2Tail, 5);
+    print(list1Head);
+    cout << "\n";
+    print(list2Head);
+    cout << "\n";
+    DoublyLinkedList *temp1 = list1Head, *temp2 = list2Tail;
+    while (temp1 != NULL && temp2 != NULL)
     {
-        if (str[i] < result)
-        {
-            result = str[i];
-        }
+        insert(&list3Head, &list3Tail, temp1->data + temp2->data);
+        temp1 = temp1->next;
+        temp2 = temp2->prev;
     }
-    cout << "Output: " << result << " with ASCII value: " << (int)result << '\n';
+    print(list3Head);
+    cout << "\n";
     return 0;
 }
