f686f58f95667a419f4c57e4d6f5fb123b9da43d
aadishjain1946 <aadishjain1946@gmail.com>
2021-09-06 17:55:22 +0530

    changes

--- test.c
+++ test.c
@@ -1,25 +1,11 @@
 #include <stdio.h>
-int processPointers(int *p, int *q, int n)
-{
-    int sum1 = 0, sum2 = 0;
-    for (int i = 0; i < n; i++)
-    {
-        sum1 += p[i];
-        sum2 += q[i];
-    }
-    if (sum1 > sum2)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
 int main()
 {
-    int a[] = {11, 55, 33, 44, 22};
-    int b[] = {1, 2, 3, 4, 5};
-    int ans = processPointers(&a[0], &b[0], 5);
-    printf("Output: %d", ans);
+    int a, b;
+    printf("Enter Input: ");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    int sum = a + b, difference = a - b, product = a * b, remainder = a % b;
+    float quotient = (float)a / (float)b;
+    printf("Output:\nSum: %d\nDifference: %d\nProduct: %d\nRemainder: %d\nQuotient: %f\n", sum, difference, product, remainder, quotient);
 }
