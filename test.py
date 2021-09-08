7596a73a06bf6b1f7354db82b39c110761a713f6
aadishjain1946 <aadishjain1946@gmail.com>
2021-09-08 17:54:27 +0530

    chnges

--- test.py
+++ test.py
@@ -1,18 +1,31 @@
-def getCharFreq(strInp):
-    output = {}
-    for i in range(len(strInp)):
-        if strInp[i].isalpha() == False:
-            continue
+class PredictCovid():
+    def diagnose(self, symptoms):
+        cnt = 0
+        for i in symptoms:
+            if symptoms[i] == "fever" or symptoms[i] == "cough" or symptoms[
+                    i] == "temperature":
+                cnt += 1
+        if cnt >= 3:
+            print("patient diagnosed with covid +")
         else:
-            if strInp[i] in output:
-                output[strInp[i]].append(i)
-            else:
-                temp = []
-                temp.append(i)
-                output[strInp[i]] = temp
-    return output
+            print("patient diagnosed with covid -")
 
 
-strInp = input()
-charDict = getCharFreq(strInp)
-print(charDict)
+class CovidPatients(PredictCovid):
+    def __init__(self, id, name, visitdate, center):
+        self.id = id
+        self.name = name
+        self.visitdate = visitdate
+        self.center = center
+        self.symptoms = []
+
+    def CollectSymptoms(self):
+        self.id = int(input("Enter your ID: "))
+        self.name = input("Enter your name: ")
+        self.symptoms = [i for i in input("Enter symptoms: ").split(" ")]
+
+    def showDetails(self):
+        print("Patient Name: ", self.name)
+        print("Patient Id: ", self.id)
+        print("Patient's Symptoms: ", self.symptoms)
+        self.diagnose(self.symptoms)
