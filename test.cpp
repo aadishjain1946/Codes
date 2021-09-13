72e277c996bf652fbe3c1562c2e9227e07519ca5
aadishjain1946 <aadishjain1946@gmail.com>
2021-09-13 22:48:50 +0530

    changes

--- test.cpp
+++ test.cpp
@@ -1,100 +1,41 @@
 #include <iostream>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
 using namespace std;
-class binarySearchTreeNode
-{
-public:
-    int data;
-    binarySearchTreeNode *left, *right;
-    binarySearchTreeNode(int data)
-    {
-        this->data = data;
-        this->left = this->right = NULL;
-    }
-};
-class binarySearchTree
-{
-private:
-    binarySearchTreeNode *root;
-    binarySearchTreeNode *insert(binarySearchTreeNode *root, int data)
-    {
-        if (root == NULL)
-        {
-            binarySearchTreeNode *temp = new binarySearchTreeNode(data);
-            return temp;
-        }
-        if (root->data > data)
-        {
-            root->left = insert(root->left, data);
-        }
-        else
-        {
-            root->right = insert(root->right, data);
-        }
-        return root;
-    }
-    void display(binarySearchTreeNode *root)
-    {
-        if (root == NULL)
-        {
-            return;
-        }
-        display(root->left);
-        cout << root->data << " ";
-        display(root->right);
-    }
-
-public:
-    binarySearchTree()
-    {
-        this->root = NULL;
-    }
-    binarySearchTree(int data)
-    {
-        this->root = NULL;
-        this->root = this->insert(this->root, data);
-    }
-    void insert(int data)
-    {
-        this->root = this->insert(this->root, data);
-    }
-    void display()
-    {
-        cout << "Elements of Binary Search Trees: ";
-        this->display(this->root);
-        cout << '\n';
-    }
-};
 int main()
 {
-    int choice = -1;
-    binarySearchTree b1;
-    while (choice != 3)
+    int n = 6;
+    string strArr[n];
+    for (int i = 0; i < n; i++)
     {
-        cout << "Menu\n";
-        cout << "1. Insert Element.\n";
-        cout << "2. Display Binary Search Tree.\n";
-        cout << "3. Exit.\n";
-        cout << "Enter your choice: ";
-        cin >> choice;
-        if (choice == 1)
+        cin >> strArr[i];
+    }
+    unordered_map<string, vector<int>> myMap;
+    for (int i = 0; i < n; i++)
+    {
+        string temp = strArr[i];
+        sort(temp.begin(), temp.end());
+        if (myMap.count(temp) == 0)
         {
-            int ele;
-            cout << "Enter element to be inserted: ";
-            cin >> ele;
-            b1.insert(ele);
-        }
-        else if (choice == 2)
-        {
-            b1.display();
-        }
-        else if (choice == 3)
-        {
-            break;
+            vector<int> sudo;
+            sudo.push_back(i);
+            myMap[temp] = sudo;
         }
         else
         {
-            cout << "Wrong choice!!\n";
+            myMap[temp].push_back(i);
         }
+    }
+    for (auto i : myMap)
+    {
+        vector<int> temp = i.second;
+        for (int j = 0; j < temp.size(); j++)
+        {
+            cout << strArr[temp[j]] << " ";
+        }
+        cout << '\n';
     }
     return 0;
 }
--- test.java
+++ test.java
@@ -1,24 +1,38 @@
 import java.util.*;
 
-public class StackTest {
+public class test {
     public static void main(String[] args)
     {
-       Stack<Integer> stack = new Stack<Integer>(); //custom stack implementation can contain some error, hence using inbuild stack
-       System.out.print("ENTER AN INTEGER VALUE(n): ");
-       int dec = new java.util.Scanner(System.in).nextInt();
-       int rem = dec%2;
-       stack.push(rem);
-       dec/=2;
-       while(dec!=0)
-       {
-           int temp = dec%2; //rem was already declared
-           stack.push(temp);
-           dec/=2;
-        }
-        System.out.print("Decimal to Binary  :");
-        while(!(stack.isEmpty())){
-            System.out.print(stack.pop());
-        }
+       Scanner ScnObj = new Scanner(System.in);
+       System.out.println("Enter first number:");
+       double num1 = ScnObj.nextDouble();
+       System.out.println("Enter second number:");
+       double num2 = ScnObj.nextDouble();
+       System.out.println("Enter a menu option");
+       System.out.println("[1] Add");
+       System.out.println("[2] Subtract");
+       System.out.println("[3] Multiply");
+       System.out.println("[4] Divide");
+       int oper = ScnObj.nextInt();
+       if(oper == 1){
+           double output = num1 + num2;
+           System.out.println(num1 + " + " + num2 + " = " + output);
+       }else if(oper == 2){
+           double output = num1 - num2;
+           System.out.println(num1 + " - " + num2 + " = " + output);
+       }else if(oper == 3){
+           double output = num1 * num2;
+           System.out.println(num1 + " * " + num2 + " = " + output);
+       }else if(oper == 4){
+           if(num2 == 0){
+               System.out.println("If Division is selected the second number cannot be zero.");
+               return;
+           }
+           double output = num1 / num2;
+           System.out.println(num1 + " / " + num2 + " = " + output);
+       }else{
+           System.out.println("Invalid menu option.");
+       }
     }
 
 }
