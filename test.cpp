b8e162ad22ac0493f3a7fc6e0d3afd08693ce4c5
aadishjain1946 <aadishjain1946@gmail.com>
2021-09-19 15:24:36 +0530

    changes

--- test.cpp
+++ test.cpp
@@ -1,22 +1,78 @@
 #include <iostream>
 using namespace std;
+class Node
+{
+public:
+    int id;
+    float cgpa;
+    string name;
+    Node *next;
+};
+void insertLL(Node **head, Node *insertNode)
+{
+    Node *temp;
+    if (*head == NULL || (*head)->id >= insertNode->id)
+    {
+        insertNode->next = *head;
+        *head = insertNode;
+    }
+    else
+    {
+        temp = *head;
+        while (temp->next != NULL && temp->next->id < insertNode->id)
+        {
+            temp = temp->next;
+        }
+        insertNode->next = temp->next;
+        temp->next = insertNode;
+    }
+}
+Node *newNode(int id, float cgpa, string name)
+{
+    Node *new_node = new Node();
+    new_node->id = id;
+    new_node->cgpa = cgpa;
+    new_node->name = name;
+    new_node->next = NULL;
+
+    return new_node;
+}
+void printLL(Node *head)
+{
+    Node *temp = head;
+    cout << "Printing Linked list: \n";
+    while (temp != NULL)
+    {
+        cout << "Id: " << temp->id << "\n";
+        cout << "Name: " << temp->name << "\n";
+        cout << "Cgpa: " << temp->cgpa << "\n";
+        temp = temp->next;
+    }
+    cout << '\n';
+}
 int main()
 {
-    int mat[2][3] = {{1, 2, 3}, {4, 5, 6}};
-    int sum = 0;
-    for (int i = 0; i < 2; i++)
+    cout << "Enter input: \n";
+    Node *head = NULL;
+    Node *nNode = NULL;
+    int id = -1;
+    float cgpa;
+    string name;
+    while (id != 0)
     {
-        for (int j = 0; j < 3; j++)
+        cout << "Enter Id: ";
+        cin >> id;
+        if (id == 0)
         {
-            sum += mat[i][j];
+            break;
         }
+        cout << "Enter Cgpa: ";
+        cin >> cgpa;
+        cout << "Enter name: ";
+        cin>>name;
+        nNode = newNode(id, cgpa, name);
+        insertLL(&head, nNode);
     }
-    cout << "Sum of element of matrix: " << sum << '\n';
-    int maxFirst = -1;
-    for (int i = 0; i < 3; i++)
-    {
-        maxFirst = max(maxFirst, mat[0][i]);
-    }
-    cout << "Max element in first row: " << maxFirst << '\n';
+    printLL(head);
     return 0;
 }
