1845122abe66990498d024664fdb350ebb9fff57
aadishjain1946 <aadishjain1946@gmail.com>
2021-09-25 16:10:42 +0530

    changes

--- test.cpp
+++ test.cpp
@@ -1,53 +1,14 @@
 #include <iostream>
 using namespace std;
-class node
-{
-public:
-    int data;
-    node *next;
-    node(int data)
-    {
-        this->data = data;
-        this->next = NULL;
-    }
-};
-node *create()
-{
-    cout << "Enter an element: ";
-    int num;
-    cin >> num;
-    node *newnode = new node(num);
-    if (num == -999)
-    {
-        return newnode;
-    }
-    node *temp = create();
-    newnode->next = temp;
-    return newnode;
-}
-void print(node *head)
-{
-    if (head == NULL)
-    {
-        return;
-    }
-    cout << head->data << " ";
-    print(head->next);
-}
-int count(node *head)
-{
-    if (head->data == -999)
-    {
-        return 0;
-    }
-    return count(head->next) + 1;
-}
 int main()
 {
-    cout << "Creating linked list:\n";
-    node *head = create();
-    cout << "\nPrinting the elements of List: ";
-    print(head);
-    cout << "\nCount of element in the linked list: " << count(head);
+    int A1[] = {0, 0, 1, 1};
+    int A2[] = {0, 1, 0, 1};
+    int n = 4;
+    cout << "A1 \t A2 \t OUT\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << A1[i] << " \t " << A2[i] << " \t " << ((A1[i] && A2[i]) || (A1[i] || A2[i])) << '\n';
+    }
     return 0;
 }
