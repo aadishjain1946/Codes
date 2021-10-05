fa62abee535708bb72141b0ba4d57664ade1f05a
aadishjain1946 <aadishjain1946@gmail.com>
2021-10-05 22:32:57 +0530

    changes

--- test.java
+++ test.java
@@ -2,25 +2,13 @@ import java.util.*;
 public class test {
     public static void main(String[] args)
     {
-        int n = 10;
-        int sum = 0,dupl = 0;
-        HashSet<Integer> set = new HashSet();
         Scanner scnObj = new Scanner(System.in);
-        for(int i = 0;i<n;i++){
-            System.out.print("Number " + (i+1) + " = ");
-            int num = scnObj.nextInt();
-            if(set.contains(num)){
-                dupl++;
-            }else{
-                set.add(num);
-                sum +=num;
-            }
-        }
-        System.out.println("Sum = "+sum);
-        if(dupl > 0){
-            System.out.println("The number of duplicates is: "+dupl);
+        System.out.print("Enter a string: ");
+        String str = scnObj.nextLine();
+        if(str.substring(0,4).equals("Java")){
+            System.out.println("Output: True");
         }else{
-            System.out.println("No duplicate numbers were entered.");
+            System.out.println("Output: False");
         }
     }
 
