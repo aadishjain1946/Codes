b8584e4a405bad9067a9cb1ae1b9bf86c9e1cd4b
aadishjain1946 <aadishjain1946@gmail.com>
2021-12-01 23:58:14 +0530

    changes

--- test.js
+++ test.js
@@ -1,5 +1,30 @@
-const checkDate = function (date) {
-    return (new Date(date) !== "Invalid Date") && !isNaN(new Date(date));
+function matchmaker(obj) {
+    function pred(input) {
+        console.log(Object.keys(obj).keys())
+        for (var x in obj) {
+            console.log(typeof (x));
+            if (obj[x] != input[x]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return pred;
 }
-var dat = new Date("2012/2/30");
-console.log(checkDate(dat));
+
+var m = matchmaker({
+    a: 'b',
+    c: 3
+});
+
+console.log(m({
+    a: 'b',
+    c: 3,
+    d: true
+}));
+console.log(m({
+    a: 'b',
+    c: 4,
+    d: true
+}));
+console.log(m(328));
