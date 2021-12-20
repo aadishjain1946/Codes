968e53e32d4ff777f1b62d7d610816c48ff0f476
aadishjain1946 <aadishjain1946@gmail.com>
2021-12-21 00:20:42 +0530

    changes

--- test.java
+++ test.java
@@ -2,29 +2,12 @@ import java.util.*;
 public class test
 {
 	public static void main(String[] args) {
-        HashSet<String> mySet = new HashSet(); 
-        String input;
         Scanner scnObj = new Scanner(System.in);
-        System.out.println("Add a name to set, use end to terminate input: ");
-        input = scnObj.nextLine();
-        while(input.compareTo("end") != 0){
-            mySet.add(input);
-            System.out.println(input + " inserted.");
-            System.out.println("Add a name to set, use end to terminate input: ");
-            input = scnObj.nextLine();
-        }
-
-        String search;
-        System.out.println("Search a name, use end to terminate searching: ");
-        search = scnObj.nextLine();
-        while(search.compareTo("end") != 0){
-            if(mySet.contains(search)){
-               System.out.println(search + " found in set.");
-            }else{
-                System.out.println(search + " not found in set.");
-            }
-            System.out.println("Search a name, use end to terminate searching: ");
-            search = scnObj.nextLine();
-        }
+        System.out.print("Enter mass of a cylinder: ");
+        int mass = scnObj.nextInt();
+        System.out.print("Enter radius of a cylinder: ");
+        int radius = scnObj.nextInt();
+        int centerOfIntertia = mass * radius * radius;
+        System.out.println("The Center of Intertia is " + centerOfIntertia);
     }
 }
--- test.py
+++ test.py
@@ -1,36 +1,49 @@
-operators = ["+", "-", "*", "/"]
+def get_month_as_int(monthString):
 
-
-def validatExpression(inputExp):
-    exp = []
-    countOp = 0
-    countEx = 0
-    for i in range(len(inputExp)):
-        if len(exp) == 0 or inputExp[i] not in operators:
-            if len(exp) == 0 or exp[-1] in operators:
-                exp.append(inputExp[i])
-                countEx += 1
-            else:
-                return False
-        elif len(exp) != 0 and inputExp[i] in operators:
-            if len(exp) != 0 and exp[-1] in operators:
-                return False
-            else:
-                exp.append(inputExp[i])
-                countOp += 1
-    if countEx > countOp:
-        return True
+    if monthString == 'January':
+        month_int = 1
+    elif monthString == 'February':
+        month_int = 2
+    elif monthString == 'March':
+        month_int = 3
+    elif monthString == 'April':
+        month_int = 4
+    elif monthString == 'May':
+        month_int = 5
+    elif monthString == 'June':
+        month_int = 6
+    elif monthString == 'July':
+        month_int = 7
+    elif monthString == 'August':
+        month_int = 8
+    elif monthString == 'September':
+        month_int = 9
+    elif monthString == 'October':
+        month_int = 10
+    elif monthString == 'November':
+        month_int = 11
+    elif monthString == 'December':
+        month_int = 12
     else:
-        return False
+        month_int = 0
+
+    return month_int
 
 
-def main():
-    inputExp = input()
-    isvalid = validatExpression(inputExp)
-    if isvalid == False:
-        print(inputExp, "is a mathematical false expression.")
+user_string = ""
+while user_string != "-1":
+    user_string = input()
+    if len(user_string.split(" ")) < 3:
+        continue
+    month, day, year = user_string.split(" ")
+    monthInt = get_month_as_int(month)
+    if monthInt == 0:
+        continue
+    if day[len(day) - 1] == ',':
+        day = day[:len(day) - 1]
     else:
-        print(inputExp, "is a mathematical true expression.")
+        continue
 
-
-main()
+    print(str(monthInt) + "/" + str(day) + "/" + str(year))
+# TODO: Read dates from input, parse the dates to find the one
+#       in the correct format, and output in m/d/yyyy format
