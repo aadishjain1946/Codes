e89c70788a0ecabd8aba3faa2345a56a7843c6a0
aadishjain1946 <aadishjain1946@gmail.com>
2021-12-23 19:22:54 +0530

    changes

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
-function Tool(name, ID, isPower, isCheckedOut) {
-    this.name = name;
-    this.ID = ID;
-    this.isPower = isPower;
-    this.isCheckedOut = isCheckedOut;
-}
-
-let hammer = new Tool("hammer", 102, false, false)
-console.log(hammer);
+`
+SELECT
+    t1.FirstName, t1.LastName
+FROM
+    Table_Employee t1
+JOIN
+    Table_Order t2
+    ON(t1.EmployeeID = t2.EmployeeID)
+WHERE
+    t2.OrderQty > 10
+`
