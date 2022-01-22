0e97d9bb90fc13578220b06f295b2c0e2b03c567
aadishjain1946 <aadishjain1946@gmail.com>
2022-01-23 00:14:20 +0530

    changes

--- test.cpp
+++ test.cpp
@@ -2,29 +2,11 @@
 using namespace std;
 int main()
 {
-    int n1, n2;
-    cout << "Enter two natural number n1 and n2: ";
-    cin >> n1 >> n2;
-    int count = 0;
-    for (int i = n1; i <= n2; i++)
-    {
-        bool flag = true;
-        if (i <= 1)
-        {
-            flag = false;
-        }
-        for (int j = 2; j < i; j++)
-        {
-            if (i % j == 0)
-            {
-                flag = false;
-            }
-        }
-        if (flag)
-        {
-            count++;
-        }
-    }
-    cout << "Number of primes between " << n1 << " and " << n2 << " are " << count << '\n';
+    int l;
+    cout << "Enter length of cube: ";
+    cin >> l;
+    float r = l / 2;
+    float residualVolume = (l * l * l) + ((4 / 3) * 3.14 * r * r * r);
+    cout << "Residual Volume: " << residualVolume << '\n';
     return 0;
 }
--- test.java
+++ test.java
@@ -4,13 +4,46 @@ public class test
     public static void main(String[] args)  
     {  
         Scanner scnObj = new Scanner(System.in);
-        int n = scnObj.nextInt();
-        ArrayList<Integer> arr = new ArrayList<>(n);
-        for(int i = 0;i<n;i++){
-            arr.add(scnObj.nextInt());
-        }
-        for(int i = arr.size() - 1;i>=0;i--){
-            System.out.println(arr.get(i));
+        System.out.print("Input charaters(maximum of 5): ");
+        StringBuilder str = new StringBuilder(scnObj.nextLine());
+        if(str.length() > 5){
+            System.out.println("You entered more than 5 characters.");
+        }else{
+            for(int i = 0;i<str.length();i++){
+                switch(str.charAt(i)){
+                    case 'a':
+                        str.setCharAt(i,'0');
+                        break;
+                    case 'e':
+                        str.setCharAt(i,'1');
+                        break;
+                    case 'i':
+                        str.setCharAt(i,'2');
+                        break;
+                    case 'o':
+                        str.setCharAt(i,'3');
+                        break;
+                    case 'u':
+                        str.setCharAt(i,'4');
+                        break;
+                    case 'A':
+                        str.setCharAt(i,'5');
+                        break;
+                    case 'E':
+                        str.setCharAt(i,'6');
+                        break;
+                    case 'I':
+                        str.setCharAt(i,'7');
+                        break;
+                    case 'O':
+                        str.setCharAt(i,'8');
+                        break;
+                    case 'U':
+                        str.setCharAt(i,'9');
+                        break;
+                }
+            }
+            System.out.println("Encrypted words: " + str);
         }
     }  
 }  
