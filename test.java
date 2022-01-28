234f4a274851af7331efe9cd430a6eb2830afdec
aadishjain1946 <aadishjain1946@gmail.com>
2022-01-29 00:41:03 +0530

    changes

--- test.java
+++ test.java
@@ -4,33 +4,26 @@ public class test
     public static void main(String[] args)  
     {  
         Scanner scnObj = new Scanner(System.in);
-        System.out.print("Enter day: ");
-        String day = scnObj.nextLine();
-        switch(day.toLowerCase()){
-            case "monday":
-                System.out.println("Not Weekend day.");
-                break;
-            case "tuesday":
-                System.out.println("Not Weekend day.");
-                break;
-            case "wednesday":
-                System.out.println("Not Weekend day.");
-                break;
-            case "thrusday":
-                System.out.println("Not Weekend day.");
-                break;
-            case "friday":
-                System.out.println("Not Weekend day.");
-                break;
-            case "saturday":
-                System.out.println("Weekend day.");
-                break;
-            case "sunday":
-                System.out.println("Weekend day.");
-                break;
-            default:
-                System.out.println("Wrong Input.");
-                break;
+        System.out.print("Enter a string: ");
+        String input = scnObj.nextLine();
+        System.out.print("Enter a pattern: ");
+        String pattern = scnObj.nextLine();
+        HashMap<Character, Character> maping = new HashMap<>();
+        boolean flag = true;
+        for(int i = 0;i<input.length();i++){
+            if(maping.containsKey(input.charAt(i))){
+                if(pattern.charAt(i) != maping.get(input.charAt(i))){
+                    flag = false;
+                    break;
+                }
+            }else{
+                maping.put(input.charAt(i),pattern.charAt(i));
+            }
+        }
+        if(flag){
+            System.out.println("Pattern matched successfully.");
+        }else{
+            System.out.println("Pattern matched failed.");
         }
     }  
 }  
