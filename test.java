0cea47093cde10929feb7712ebdfc4b69dc3bf32
aadishjain1946 <aadishjain1946@gmail.com>
2022-02-13 22:50:12 +0530

    changes

--- test.java
+++ test.java
@@ -1,13 +1,25 @@
 import java.util.*;
-import java.lang.Math;
 public class test  
 {  
     public static void main(String[] args)  
     {  
         Scanner scnObj = new Scanner(System.in);
-        System.out.print("Enter p: ");
-        double p = scnObj.nextDouble();
-        double output = p*(Math.sin(p)) + p*(Math.cos(p));
-        System.out.println("Output: " + output);
+        System.out.print("Enter age: ");
+        int age = scnObj.nextInt();
+        if(age < 13){
+            System.out.println("Too young to create a Facebook account");
+        }else if(age < 16){
+            System.out.println("Too young to get a driver's licence.");
+        }else if(age < 18){
+            System.out.println("Too young to get a tattoo.");
+        }else if(age < 21){
+            System.out.println("Too young to dirnk alcohol");
+        }else if(age < 35){
+            System.out.println("Too young to run for the President of the U.S");
+            System.out.println("How sad!");
+        }else{
+            System.out.println("able to do anything");
+            System.out.println("How happy!");
+        }
     }  
 }  
