01d4cb69297f1dacf667aee8dca4f13e0fc4788d
aadishjain1946 <aadishjain1946@gmail.com>
2022-02-16 00:13:48 +0530

    changes

--- test.py
+++ test.py
@@ -1,34 +1,17 @@
-class bankAccount:
-    def __init__(self, accountNo, balance, interestRate):
-        self.accountNo = accountNo
-        self.balance = balance
-        self.interestRate = interestRate
-
-    def print(self):
-        print("Account Number: ", self.accountNo)
-        print("Balance: ", self.balance)
-        print("Interest Rate: ", self.interestRate)
-
-
-class bank:
-    def __init__(self, accounts):
-        self.accounts = accounts
-
-    def sortAccount(self):
-        sortedAccountList = sorted(self.accounts, key=lambda x: x.accountNo)
-        for i in sortedAccountList:
-            i.print()
-            print()
-
-
 def main():
-    bnk = bank([
-        bankAccount(1, 1000, 5),
-        bankAccount(3, 2000, 5.5),
-        bankAccount(10, 50000, 4.4),
-        bankAccount(9, 4450, 57)
-    ])
-    bnk.sortAccount()
+    inputList = [1, 2, 3, 4, 5]
+    print("Input List: ", inputList)
+    myMap = {}
+    for i in inputList:
+        if i not in myMap:
+            myMap[i] = 2
+        else:
+            myMap[i] += 2
+    output = []
+    for i in myMap:
+        for j in range(myMap[i]):
+            output.append(i)
+    print("Output List: ", output)
 
 
 if __name__ == "__main__":
