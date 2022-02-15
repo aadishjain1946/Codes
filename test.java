ccd6364c1516136d205edfd2d65010df80011bd6
aadishjain1946 <aadishjain1946@gmail.com>
2022-02-15 09:04:05 +0530

    changes

--- test.java
+++ test.java
@@ -1,25 +1,32 @@
 import java.util.*;
+import java.time.LocalTime;
 public class test  
-{  
+{ 
     public static void main(String[] args)  
     {  
         Scanner scnObj = new Scanner(System.in);
-        System.out.print("Enter age: ");
-        int age = scnObj.nextInt();
-        if(age < 13){
-            System.out.println("Too young to create a Facebook account");
-        }else if(age < 16){
-            System.out.println("Too young to get a driver's licence.");
-        }else if(age < 18){
-            System.out.println("Too young to get a tattoo.");
-        }else if(age < 21){
-            System.out.println("Too young to dirnk alcohol");
-        }else if(age < 35){
-            System.out.println("Too young to run for the President of the U.S");
-            System.out.println("How sad!");
+        String input = scnObj.nextLine();
+        String cur1 = input.split(";")[0];
+        String cur2 = input.split(";")[1].split("\\|")[0];
+        String rcur1 = input.split(";")[1].split("\\|")[1].substring(0,3);
+        String rcur2 = input.split(";")[1].split("\\|")[1].substring(3,6);
+        if(cur1.contains(rcur1)){
+            if(cur2.contains(rcur2)){
+                double ans = Double.parseDouble(cur1.split(":")[1]) * Double.parseDouble(cur2.split(":")[1]);
+                System.out.println((input.split(";")[1].split("\\|")[1]) + ": " + ans);
+            }else{
+                System.out.println("Unable to determine rate for " + (input.split(";")[1].split("\\|")[1]));
+            }
+        }else if(cur1.contains(rcur2)){
+            if(cur2.contains(rcur1)){
+                double ans = Double.parseDouble(cur1.split(":")[1]) * Double.parseDouble(cur2.split(":")[1]);
+                System.out.println((input.split(";")[1].split("\\|")[1]) + ": " + ans);
+            }else{
+                System.out.println("Unable to determine rate for " + (input.split(";")[1].split("\\|")[1]));
+            }
         }else{
-            System.out.println("able to do anything");
-            System.out.println("How happy!");
+            System.out.println("Unable to determine rate for " + (input.split(";")[1].split("\\|")[1]));
         }
     }  
+    
 }  
