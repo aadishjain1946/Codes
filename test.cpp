31747252429ad604f25bf0d1fc5abf445c0a1f0a
aadishjain1946 <aadishjain1946@gmail.com>
2022-02-22 08:36:16 +0530

    changes

--- test.cpp
+++ test.cpp
@@ -1,44 +1,10 @@
 #include <iostream>
 using namespace std;
-class Node
-{
-public:
-    int data;
-    int priority;
-    Node *next;
-    Node(int data, int priority)
-    {
-        this->data = data;
-        this->priority = priority;
-        this->next = NULL;
-    }
-};
-void deQueue(Node **front)
-{
-    Node *sudo = *front;
-    (*front) = (*front)->next;
-    free(sudo);
-}
-void enQueue(Node **front, int data, int priority)
-{
-    Node *temp = (*front);
-    Node *sudo = new Node(data, priority);
-    if ((*front)->priority > priority)
-    {
-        sudo->next = *front;
-        (*front) = sudo;
-    }
-    else
-    {
-        while (temp->next != NULL && temp->next->priority < priority)
-        {
-            temp = temp->next;
-        }
-        sudo->next = temp->next;
-        temp->next = sudo;
-    }
-}
 int main()
 {
+    int n = 0;
+    cout << "Enter input: ";
+    cin >> n;
+    cout << "Output: " << 10 * n;
     return 0;
 }
