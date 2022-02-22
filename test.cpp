2c356d796fa97d240beb49c394adf24b5072b7d7
aadishjain1946 <aadishjain1946@gmail.com>
2022-02-23 00:04:56 +0530

    changes made in c++ && java

--- test.cpp
+++ test.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 using namespace std;
+class box
+{
+    int height, width, depth;
+
+public:
+    box(int height, int width, int depth)
+    {
+        this->height = height;
+        this->width = width;
+        this->depth = depth;
+    }
+    int volume()
+    {
+        return height * width * depth;
+    }
+};
 int main()
 {
-    int n = 0;
-    cout << "Enter input: ";
-    cin >> n;
-    cout << "Output: " << 10 * n;
+    box b1(10, 20, 30);
+    box b2(4, 7, 9);
+    cout << "Volume of box 1: " << b1.volume() << '\n';
+    cout << "Volume of box 2: " << b2.volume();
     return 0;
 }
--- test.java
+++ test.java
@@ -1,55 +1,21 @@
+import java.lang.*;
 import java.util.*;
-public class test  
-{ 
-    public static class Student{
-        private String studentName;
-        private int studentNumber;
-        private long regYear;
-        private String faculty;
-        private String department;
-        public String getDepartment(){
-            return department;
-        }
-        public String getFaculty(){
-            return faculty;
-        }
-        public String getStudentName(){
-            return studentName;
-        }
-        public int getStudentNumber(){
-            return studentNumber;
-        }
-        public long getRegYear(){
-            return regYear;
-        }
-        public void setRegYear(long regYear){
-            this.regYear = regYear;
-        }
-        public void setStudentNumber(int studentNumber){
-            this.studentNumber = studentNumber;
-        }
-        public void setDepartment(String department){
-            this.department = department;
-        }
-        public void setFaculty(String faculty){
-            this.faculty = faculty;
-        }
-        public void setStudentName(String studentName){
-            this.studentName = studentName;
-        }
-        public String toString(){
-            String output = studentName + ", " + studentNumber + ", " + faculty;
-            return output;
-        }
-    }
-    public static void main(String[] args)  
-    {  
-        Student s = new Student();
-        s.setDepartment("abc");
-        s.setFaculty("XYZ");
-        s.setRegYear(12121);
-        s.setStudentName("John");
-        s.setStudentNumber(423);
-        System.out.println(s.toString());
-    }  
-}  
+public class test {
+
+	public static int allPossibleSteps(int num)
+	{
+		if (num == 0)
+			return 1;
+		else if (num < 0)
+			return 0;
+		else
+			return allPossibleSteps(num - 3) + allPossibleSteps(num - 2)
+				+ allPossibleSteps(num - 1);
+	}
+
+	public static void main(String argc[])
+	{
+		int num = 10;
+		System.out.println("All Possible Steps: " + allPossibleSteps(num));
+	}
+}
