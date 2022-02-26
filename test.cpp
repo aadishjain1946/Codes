59b6c1c97556e2cc2bb182d474c8e6c2c4c9faa1
aadishjain1946 <aadishjain1946@gmail.com>
2022-02-26 23:43:21 +0530

    changes made in c++ && java

--- test.cpp
+++ test.cpp
@@ -1,26 +1,18 @@
 #include <iostream>
 using namespace std;
-class box
+void printInformation(string fullName, int matrixNo, string group)
 {
-    int height, width, depth;
-
-public:
-    box(int height, int width, int depth)
-    {
-        this->height = height;
-        this->width = width;
-        this->depth = depth;
-    }
-    int volume()
-    {
-        return height * width * depth;
-    }
-};
+    cout << "Full Name: " << fullName << '\n';
+    cout << "Matrix Number: " << matrixNo << "\n";
+    cout << "Group: " << group << '\n';
+}
+void plagarismStatement()
+{
+    cout << "I confirm that this LAB TEST is my own work and is not copied.\n";
+}
 int main()
 {
-    box b1(10, 20, 30);
-    box b2(4, 7, 9);
-    cout << "Volume of box 1: " << b1.volume() << '\n';
-    cout << "Volume of box 2: " << b2.volume();
+    printInformation("Alan Luna", 123, "A");
+    plagarismStatement();
     return 0;
 }
--- test.py
+++ test.py
@@ -1,17 +1,8 @@
 def main():
-    inputList = [1, 2, 3, 4, 5]
-    print("Input List: ", inputList)
-    myMap = {}
-    for i in inputList:
-        if i not in myMap:
-            myMap[i] = 2
-        else:
-            myMap[i] += 2
-    output = []
-    for i in myMap:
-        for j in range(myMap[i]):
-            output.append(i)
-    print("Output List: ", output)
+    kineticEnergy = float(input("Enter Kinetic Energy: "))
+    totalEnergy = float(input("Enter Total Energy: "))
+    potentialEnergy = totalEnergy - kineticEnergy
+    print("Total Energy:", potentialEnergy)
 
 
 if __name__ == "__main__":
