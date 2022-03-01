3e2b968382c0a3d024f8ec8707061057fe7ef146
aadishjain1946 <aadishjain1946@gmail.com>
2022-03-01 23:43:49 +0530

    changes made in c++ && java

--- test.cpp
+++ test.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
 using namespace std;
-void printInformation(string fullName, int matrixNo, string group)
-{
-    cout << "Full Name: " << fullName << '\n';
-    cout << "Matrix Number: " << matrixNo << "\n";
-    cout << "Group: " << group << '\n';
-}
-void plagarismStatement()
-{
-    cout << "I confirm that this LAB TEST is my own work and is not copied.\n";
-}
 int main()
 {
-    printInformation("Alan Luna", 123, "A");
-    plagarismStatement();
+    int count = 1, sum = 0;
+    for (; count <= 99; count++)
+    {
+        sum += count;
+    }
+    cout << "Sum: " << sum << '\n';
     return 0;
 }
