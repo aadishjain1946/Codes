58cee9f432079d9204db352d2c3ef43c07846d29
aadishjain1946 <aadishjain1946@gmail.com>
2022-03-12 23:55:19 +0530

    changes made in c++ && java

--- test.cpp
+++ test.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 using namespace std;
-int power(int x, int n)
+class node
 {
-    if (n == 1)
-    {
-        return x;
-    }
-    return x * power(x, n - 1);
-}
+public:
+    int num;
+    node *next;
+};
 int main()
 {
-    int num = power(2, 4);
-    cout << "2 power 4: " << num << '\n';
+    node *head = new node();
+    node *n1 = new node();
+    node *n2 = new node();
+    cout << "Enter 3 node: ";
+    cin >> head->num;
+    cin >> n1->num;
+    cin >> n2->num;
+    head->next = n1;
+    n1->next = n2;
+    n2->next = NULL;
+    node *temp = head;
+    cout << "Output: ";
+    while (temp)
+    {
+        cout << temp->num << " ";
+        temp = temp->next;
+    }
     return 0;
 }
--- test.py
+++ test.py
@@ -1,9 +1,22 @@
 def main():
-    word1, word2 = input("Enter words: ").split(" ")
-    if word1 == word2:
-        print("Super Anagram!")
+    s = int(input("Enter the value of s: "))
+    arr = [int(i) for i in input().split(" ")]
+    if s == 1:
+        sum = 0
+        for i in range(len(arr)):
+            if arr[i] >= 6 and arr[i] <= 60:
+                sum += arr[i]
+        print("Sum of numbers between 6 and 60:", sum)
+    elif s == 2:
+        sum = 0
+        for i in range(len(arr)):
+            if arr[i] >= 0:
+                sum += arr[i]
+        print("Sum of positive number:", sum)
     else:
-        print("Huh?")
+        for i in range(len(arr)):
+            if arr[i] % 4 == 0:
+                print(arr[i], end=" ")
 
 
 if __name__ == "__main__":
