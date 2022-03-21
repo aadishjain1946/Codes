7d09c1a47bb40a9c0375aca1c7e188a803703191
aadishjain1946 <aadishjain1946@gmail.com>
2022-03-22 00:23:18 +0530

    changes made in c++ && java

--- test.py
+++ test.py
@@ -1,9 +1,29 @@
-str1 = input("Enter string 1: ")
-str2 = input("Enter string 2: ")
+n = int(input("Enter the number of element in the list:"))
+arr = []
+for i in range(n):
+    arr.append(input("Enter element" + str(i + 1) + ":"))
+print(arr)
+removeWord = input("Enter a word to remove: ")
+occurence = int(input("Enter the occurence to remove: "))
+count = 0
+freq = {}
+freqSet = set()
+output = []
+for i in range(len(arr)):
+    freqSet.add(arr[i])
+    if arr[i] in freq:
+        freq[arr[i]] += 1
+    else:
+        freq[arr[i]] = 1
 
-x1 = len(str1)
-x2 = len(str2)
+    if arr[i] == removeWord:
+        count += 1
 
-output = x1**3 - x2**2 + x1 * x2 - 5
-
-print("Output:", output)
+    if count == occurence:
+        print("Enter")
+        count = 0
+        continue
+    output.append(arr[i])
+print("The number of repetition is:", freq[removeWord])
+print("Updated list is", output)
+print("The distinct element are:", freqSet)
