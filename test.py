268336013c00ecb060fc2ed4faf7d0e1816bcb01
aadishjain1946 <aadishjain1946@gmail.com>
2022-03-23 23:20:14 +0530

    changes made in c++ && java

--- test.py
+++ test.py
@@ -1,14 +1,58 @@
-countUpperCase = 0
-countLowerCase = 0
-with open("input.txt", "r") as txt_file:
-    fileContent = txt_file.readlines()
-    for i in range(len(fileContent)):
-        text = fileContent[i]
-        for j in range(len(text)):
-            if text[j].isupper():
-                countUpperCase += 1
-            else:
-                countLowerCase += 1
+N = int(input("Enter n: "))
 
-print("Count of upper case letters:", countUpperCase)
-print("Count of lower case letters:", countLowerCase)
+
+def printNQueenSolution(chessBoard):
+    for i in range(N):
+        for j in range(N):
+            print(chessBoard[i][j], end=" ")
+        print()
+
+
+def safeMoveCheck(chessBoard, numRow, numCol):
+
+    for i in range(numCol):
+        if chessBoard[numRow][i] == 1:
+            return False
+
+    for i, j in zip(range(numRow, -1, -1), range(numCol, -1, -1)):
+        if chessBoard[i][j] == 1:
+            return False
+
+    for i, j in zip(range(numRow, N, 1), range(numCol, -1, -1)):
+        if chessBoard[i][j] == 1:
+            return False
+
+    return True
+
+
+def nQueenProblemUtil(chessBoard, numCol):
+
+    if numCol >= N:
+        return True
+
+    for i in range(N):
+
+        if safeMoveCheck(chessBoard, i, numCol):
+
+            chessBoard[i][numCol] = 1
+
+            if nQueenProblemUtil(chessBoard, numCol + 1) == True:
+                return True
+
+            chessBoard[i][numCol] = 0
+
+    return False
+
+
+def main():
+    chessBoard = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
+    if nQueenProblemUtil(chessBoard, 0) == False:
+        print("No Solution!!")
+        return
+
+    printNQueenSolution(chessBoard)
+
+
+if __name__ == "__main__":
+    main()
