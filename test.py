e711579715c279baf06b5eb771d691edee297db8
aadishjain1946 <aadishjain1946@gmail.com>
2022-03-23 08:05:52 +0530

    changes made in c++ && java

--- test.py
+++ test.py
@@ -1,29 +1,14 @@
-n = int(input("Enter the number of element in the list:"))
-arr = []
-for i in range(n):
-    arr.append(input("Enter element" + str(i + 1) + ":"))
-print(arr)
-removeWord = input("Enter a word to remove: ")
-occurence = int(input("Enter the occurence to remove: "))
-count = 0
-freq = {}
-freqSet = set()
-output = []
-for i in range(len(arr)):
-    freqSet.add(arr[i])
-    if arr[i] in freq:
-        freq[arr[i]] += 1
-    else:
-        freq[arr[i]] = 1
+countUpperCase = 0
+countLowerCase = 0
+with open("input.txt", "r") as txt_file:
+    fileContent = txt_file.readlines()
+    for i in range(len(fileContent)):
+        text = fileContent[i]
+        for j in range(len(text)):
+            if text[j].isupper():
+                countUpperCase += 1
+            else:
+                countLowerCase += 1
 
-    if arr[i] == removeWord:
-        count += 1
-
-    if count == occurence:
-        print("Enter")
-        count = 0
-        continue
-    output.append(arr[i])
-print("The number of repetition is:", freq[removeWord])
-print("Updated list is", output)
-print("The distinct element are:", freqSet)
+print("Count of upper case letters:", countUpperCase)
+print("Count of lower case letters:", countLowerCase)
