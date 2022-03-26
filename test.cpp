201d8876e70716ebd54fef45167cc385978c2745
aadishjain1946 <aadishjain1946@gmail.com>
2022-03-26 22:51:53 +0530

    changes

--- test.cpp
+++ test.cpp
@@ -1,32 +1,16 @@
 #include <iostream>
 using namespace std;
-bool no_zeros(int arr[], int n)
+long factorial(long n)
 {
-    int count = 0;
-    for (int i = 0; i < n; i++)
+    long fact = (2 * n - 1);
+    for (long i = fact - 1; i > 0; i--)
     {
-        if (arr[i] == 0)
-        {
-            for (int j = i; j < n - 1; j++)
-            {
-                arr[j] = arr[j + 1];
-            }
-            count++;
-            i--;
-        }
+        fact *= i;
     }
-    cout << "Output: ";
-    for (int i = 0; i < n - count; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << '\n';
-    return ((count > 0) ? true : false);
+    return fact;
 }
 int main()
 {
-    int arr[] = {5, 4, 0, 0, 1, 2, 3, 0, 6, 7, 0, 8, 0, 9};
-    bool out = no_zeros(arr, 14);
-    cout << ((out) ? "True" : "False");
+    cout << "Factorial: " << factorial(5);
     return 0;
 }
