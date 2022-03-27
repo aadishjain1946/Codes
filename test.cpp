c83832c126fbc2341fb9a40ae5d08027071c399e
aadishjain1946 <aadishjain1946@gmail.com>
2022-03-27 22:32:36 +0530

    changes

--- test.cpp
+++ test.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 using namespace std;
-long factorial(long n)
-{
-    long fact = (2 * n - 1);
-    for (long i = fact - 1; i > 0; i--)
-    {
-        fact *= i;
-    }
-    return fact;
-}
 int main()
 {
-    cout << "Factorial: " << factorial(5);
+    string str;
+    cout << "Input: ";
+    cin >> str;
+    string output = "";
+    for (int i = 0; i < str.length(); i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            output += str[i];
+        }
+    }
+    cout << "Output: " << output;
     return 0;
 }
