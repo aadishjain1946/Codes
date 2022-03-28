f0127ce4fa159ac4115ccd67d46731d4ce23fa45
aadishjain1946 <aadishjain1946@gmail.com>
2022-03-29 00:15:46 +0530

    changes

--- test.py
+++ test.py
@@ -1,57 +1,12 @@
-N = int(input("Enter n: "))
-
-
-def printNQueenSolution(chessBoard):
-    for i in range(N):
-        for j in range(N):
-            print(chessBoard[i][j], end=" ")
-        print()
-
-
-def safeMoveCheck(chessBoard, numRow, numCol):
-
-    for i in range(numCol):
-        if chessBoard[numRow][i] == 1:
-            return False
-
-    for i, j in zip(range(numRow, -1, -1), range(numCol, -1, -1)):
-        if chessBoard[i][j] == 1:
-            return False
-
-    for i, j in zip(range(numRow, N, 1), range(numCol, -1, -1)):
-        if chessBoard[i][j] == 1:
-            return False
-
-    return True
-
-
-def nQueenProblemUtil(chessBoard, numCol):
-
-    if numCol >= N:
-        return True
-
-    for i in range(N):
-
-        if safeMoveCheck(chessBoard, i, numCol):
-
-            chessBoard[i][numCol] = 1
-
-            if nQueenProblemUtil(chessBoard, numCol + 1) == True:
-                return True
-
-            chessBoard[i][numCol] = 0
-
-    return False
+def replaceFile(id, image):
+    fileName = image.split(".")
+    outputFileName = id + "." + fileName[1]
+    return (fileName[0], outputFileName)
 
 
 def main():
-    chessBoard = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-
-    if nQueenProblemUtil(chessBoard, 0) == False:
-        print("No Solution!!")
-        return
-
-    printNQueenSolution(chessBoard)
+    output = replaceFile("dog", "hibiscus.bmp")
+    print("Id:", output[0], "Image:", output[1])
 
 
 if __name__ == "__main__":
