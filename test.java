2798e246c3eb8a9e9d65320abddfae419754e009
aadishjain1946 <aadishjain1946@gmail.com>
2022-04-05 23:50:47 +0530

    changes

--- test.java
+++ test.java
@@ -1,15 +1,27 @@
 import java.util.*;
 public class test {
+	public static class AddOneToArrayList{
+		public static ArrayList<Integer> solution(ArrayList<Integer> list){
+			// ↓↓↓↓ your code goes here ↓↓↓↓
+			int carry = 1;
+			for(int i = 2; i > 0; i--){
+				int sum = list.get(i) + carry;
+				carry = sum /10;
+				list.set(i, sum % 10);
+			}
+			if(carry > 0){
+				list.add(0,carry);
+			}
+			return list;
+		}
+	}
 	public static void main(String argc[])
 	{
-		try{
-			Scanner scnObj = new Scanner(System.in);
-			System.out.print("Enter two integers: ");
-			int n1 = scnObj.nextInt();
-			int n2 = scnObj.nextInt();
-			System.out.println("Sum of two integers: " + (n1+n2));
-		}catch(InputMismatchException e){
-			System.out.println("Input Mismatch!");
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		Scanner scnObj = new Scanner(System.in);
+		for(int i = 0; i < 3; i++){
+			list.add(scnObj.nextInt());
 		}
+		AddOneToArrayList.solution(list);
 	}
 }
--- test.py
+++ test.py
@@ -1,9 +1,18 @@
+import math
+
+
 def main():
-    myString = input()
-    first = myString.split("(")[1]
-    second = first.split(")")[0]
-    result = "(" + second + ")"
-    print("Output:", result)
+    for i in range(1, 6):
+        print("Cos", i, "->", math.cos(i))
+        print("Sin", i, "->", math.sin(i))
+        print("tan", i, "->", math.tan(i))
+        print("degrees", i, "->", math.degrees(i))
+        print("radians", i, "->", math.radians(i))
+        print()
+
+    str = "ILOVENVSU"
+    asciiStr = [ord(c) for c in str]
+    print(str, "to ASCII:", asciiStr)
 
 
 if __name__ == "__main__":
