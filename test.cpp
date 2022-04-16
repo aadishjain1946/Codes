cacffa03afdf1be5a4635dbaa43ffb3cc2d9386d
aadishjain1946 <aadishjain1946@gmail.com>
2022-04-16 22:46:36 +0530

    changes

--- test.cpp
+++ test.cpp
@@ -2,13 +2,60 @@
 using namespace std;
 int main()
 {
-    cout << "Even numbers(2 - 128) are:\n";
-    for (int i = 2; i <= 128; i++)
+    char grade;
+    float sum = 0.0;
+    float count = 0.0;
+    cout << "Input: \n";
+    do
     {
-        if (i % 2 == 0)
+        cin >> grade;
+        switch (grade)
         {
-            cout << i << " ";
+        case 'A':
+            sum += 4.0;
+            count++;
+            break;
+        case 'B':
+            sum += 3.0;
+            count++;
+            break;
+        case 'C':
+            sum += 2.0;
+            count++;
+            break;
+        case 'D':
+            sum += 1.0;
+            count++;
+            break;
+        case 'F':
+            sum += 0.0;
+            count++;
+            break;
+        default:
+            break;
         }
+    } while (grade >= 'A' && grade <= 'F' && grade != 'E');
+    float average = int(sum / count);
+    cout << "Output: ";
+    if (average == 4)
+    {
+        cout << "A";
+    }
+    else if (average == 3)
+    {
+        cout << "B";
+    }
+    else if (average == 2)
+    {
+        cout << "C";
+    }
+    else if (average == 1)
+    {
+        cout << "D";
+    }
+    else
+    {
+        cout << "F";
     }
     return 0;
 }
