c196f19b139ccce5077a277d21ba440d14624bc3
aadishjain1946 <aadishjain1946@gmail.com>
2022-05-16 22:09:48 +0530

    changes

--- test.cpp
+++ test.cpp
@@ -1,24 +1,35 @@
 #include <iostream>
 using namespace std;
+class complex
+{
+    int x, y;
+
+public:
+    complex() {}
+    complex(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+    complex operator-(complex c1)
+    {
+        complex result(this->x - c1.x, this->y - c1.y);
+        return result;
+    }
+    void print()
+    {
+        cout << this->x << " + (" << this->y << ")i\n";
+    }
+};
 int main()
 {
-    int a, b;
-    cin >> a >> b;
-    string numbers[] = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-    for (int i = a; i <= b; i++)
-    {
-        if (i <= 9 && i >= 1)
-        {
-            cout << numbers[i - 1] << " ";
-        }
-        else if (i % 2 == 0)
-        {
-            cout << "Even ";
-        }
-        else
-        {
-            cout << "Odd ";
-        }
-    }
+    complex c1(50, 4), c2(18, 10);
+    cout << "Complex 1: ";
+    c1.print();
+    cout << "Complex 2: ";
+    c2.print();
+    complex c3 = c1 - c2;
+    cout << "Complex c1 - c2 = ";
+    c3.print();
     return 0;
 }
